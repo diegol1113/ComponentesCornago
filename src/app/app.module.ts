d20201d8122211ba1f87bf312a20dd252e6d5dd9
diegol1113 <diegojosecornago@gmail.com>
2022-09-16 16:31:50 -0300

    primer commit actividad clase2 coderhouse angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrimeContentComponent } from './components/prime-content/prime-content.component';
+import { LeftContentComponent } from './components/left-content/left-content.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeContentComponent,
+    LeftContentComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/prime-content/prime-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prime-content',
+  templateUrl: './prime-content.component.html',
+  styleUrls: ['./prime-content.component.css']
+})
+export class PrimeContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
